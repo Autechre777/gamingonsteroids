918e97615598c317b9f5e2f038e0e418e7b90b8b
Autechre777 <Autechre777@gmail.com>
2018-04-08 07:12:42 -0700

    Update TRUStInMyViktor.lua

--- TRUStInMyViktor.lua
+++ TRUStInMyViktor.lua
@@ -108,11 +108,10 @@ end
 
 function Viktor:Tick()
 	DontAAPassive = self.Menu.Combo.qAuto:Value()
-	if myHero.attackData.endTime - Game.Timer() - myHero.attackData.windDownTime + .5 > .1 then 
+	if myHero.attackData.endTime - Game.Timer() - myHero.attackData.windDownTime + .3 > .1 then 
 	  SetMovement(false)
 	  end
-	if myHero.attackData.endTime - Game.Timer() - myHero.attackData.windDownTime + .5 < .1 and
-	  castSpell.state == 0 then
+	if myHero.attackData.endTime - Game.Timer() - myHero.attackData.windDownTime + .3 < .1 then
 	  SetMovement(true)
 	  end
 	if (_G.EOW) then
